698c43b98b6822921027bfc1aacde1268578668e
Emmanuel Carrón <emmanuel.carron@itscordoba.edu.ar>
2020-09-15 20:50:53 -0300

    Añadido toggleToast

--- src/app/modal-settings/modal-settings.page.ts
+++ src/app/modal-settings/modal-settings.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal-settings',
@@ -10,18 +10,30 @@ export class ModalSettingsPage implements OnInit {
 
   @Input() public lista2: Array<any>;
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController,
+              private toastCtrl: ToastController,
+              ) { }
 
   ngOnInit() {
   }
 
+  async presentToast(mensaje: string) {
+    const toast = await this.toastCtrl.create({
+      message: mensaje,
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  actualizarValor(){
+    console.log(this.lista2);
+    this.presentToast('Cambio realizado');
+  }
   cerrarModal(){
     this.modalCtrl.dismiss();
   }
   
 
-  actualizarValor(){
-    console.log(this.lista2)
-  }
+  
 
 }
